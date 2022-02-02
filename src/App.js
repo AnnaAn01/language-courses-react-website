a58aac57be425d243dfb3661e2f03bbec77816e8
Anna An <arzumanyan.an@gmail.com>
2022-02-02 15:03:19 +0400

    Added Routes to the App.js file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import Home from "./pages";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
+import SigninPage from "./pages/signin";
 
 function App() {
   return (
     <div>
       <Router>
-        <Home />
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+
+          <Route path="/signin" element={<SigninPage />} />
+        </Routes>
       </Router>
     </div>
   );
