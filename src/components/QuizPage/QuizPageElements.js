344405232eb41adc999d898e623b2d96cde85f46
Anna An <arzumanyan.an@gmail.com>
2022-05-04 11:07:48 +0400

    creating the quiz game

--- src/components/QuizPage/QuizPageElements.js
+++ src/components/QuizPage/QuizPageElements.js
@@ -1,2 +1,7 @@
 import styled from "styled-components";
-import { Link } from "react-router-dom";
+import { Link as LinkR } from "react-router-dom";
+
+export const QuizPageContainer = styled.div``;
+export const QuizWrapper = styled.div``;
+export const QuizH1 = styled.h1``;
+export const QuizLink = styled(LinkR)``;
--- src/components/QuizPage/index.js
+++ src/components/QuizPage/index.js
@@ -1,7 +1,20 @@
 import React from "react";
+import {
+  QuizPageContainer,
+  QuizWrapper,
+  QuizH1,
+  QuizLink,
+} from "./QuizPageElements";
 
 const QuizPage = () => {
-  return <div>QuizPage</div>;
+  return (
+    <QuizPageContainer>
+      <QuizWrapper>
+        <QuizH1>Are You Ready?</QuizH1>
+        <QuizLink to="/quizgame">Take the quiz</QuizLink>
+      </QuizWrapper>
+    </QuizPageContainer>
+  );
 };
 
 export default QuizPage;
